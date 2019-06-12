1ac46adf930d751f5389aa9cc65b81e23e73593b
wangyongyue <wangyouyue@kingbirdplus.com>
2019-06-12 15:16:48 +0800

    Initial Commit

---
+++ swift-struct-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-struct-demo
+//
+//  Created by apple on 2019/6/12.
+//  Copyright © 2019 wyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
